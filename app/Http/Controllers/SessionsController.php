da3bf3b6c76c1dbdbff1ab40345a0bea06e12025
= <martinischweppes@gmail.com>
2022-07-13 22:07:25 +0200

    jestem zmęczony4

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,7 +8,7 @@ class SessionsController extends Controller
 {
     public function create()
     {
-        return view('/');
+        return view('auth.login');
     }
 
     public function store()
